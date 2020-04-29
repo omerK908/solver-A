acf6d7dda392e52d574962e3c3c2da5774106962
Omer Kalif <omer908@gmail.com>
2020-04-29 16:59:54 +0300

    compilation works

--- solver.cpp
+++ solver.cpp
@@ -8,183 +8,158 @@
 
 using namespace solver;
 
-bool solver::isZero(double num)
-{
-    return num <= EPS && num >= -EPS;
+bool solver::isZero(double x){
+    return (x <= EPS && x >= -EPS);
 }
 
-const double solver::solve(RealVariable& x){
-    
-    return 1.23;
+/*********RealVariable*********/
 
-}
-const std::complex<double> solver::solve(ComplexVariable& x){
-    return 1.23;
-
+RealVariable& RealVariable::operator==(double x){
+    return *this;
 }
 
-/*operators*/
-
-RealVariable& solver::operator+(RealVariable& x,RealVariable& y){
-    if(x._power==y._power) x._coef=x._coef+y._coef;
-    
-    return x;
-
-}
-RealVariable& solver::operator+(RealVariable& x,int y){
-    
-    return x;
-
-}
-RealVariable& solver::operator+(int y,RealVariable& x){
-    
-    return x;
-
+RealVariable& RealVariable::operator==(RealVariable &x){
+    return *this;
 }
 
-RealVariable& solver::operator*(int x,RealVariable& y){
-    
+RealVariable& RealVariable::operator+(double x){
+    return *this;
+}
+
+RealVariable& RealVariable::operator+(RealVariable &x){
+    return *this;
+}
+
+RealVariable& RealVariable::operator-(double x){
+    return *this;
+}
+
+RealVariable& RealVariable::operator-(RealVariable &x){
+    return *this;
+}
+
+RealVariable& RealVariable::operator*(double x){
+    this->_coef *= x;
+    return *this;
+}
+
+RealVariable& RealVariable::operator*(RealVariable &x){
+    this->_coef *= x._coef;
+    this->_power *= x._power;
+    return *this;
+}
+
+RealVariable& RealVariable::operator^(double x){
+    this->_power *= x;
+    return *this;
+}
+
+RealVariable& RealVariable::operator/(double x){
+    if(isZero(x)) throw runtime_error("can not divide by zero");
+    this->_coef /= x;
+    return *this;
+}
+
+RealVariable& RealVariable::operator/(RealVariable &x){
+    if(isZero(x._coef)) throw runtime_error("can not divide by zero");
+    this->_coef /= x._coef;
+    this->_power -= x._power;
+    return *this;
+}
+
+RealVariable& solver::operator==(double x, RealVariable &y){
     return y;
 }
-RealVariable& solver::operator*(RealVariable& x,RealVariable& y){
-    
+
+RealVariable& solver::operator+(double x, RealVariable &y){
     return y;
 }
-RealVariable& solver::operator*(RealVariable& y,int x){
-    
+
+RealVariable& solver::operator-(double x, RealVariable &y){
     return y;
 }
-RealVariable& solver::operator/(RealVariable& x,double y){
-    
-    return x;
 
-}
-RealVariable& solver::operator/(RealVariable& x,RealVariable& y){
-    
-    return x;
-
-}
-RealVariable& solver::operator/(double x,RealVariable& y){
-    
-    return y;
-
-}
-RealVariable& solver::operator-(RealVariable& x,const int y){
-    
-    return x;
-
-
-}
-RealVariable& solver::operator-(const int x,RealVariable& y){
-    
+RealVariable& solver::operator*(double x, RealVariable &y){
     return y;
 }
-RealVariable& solver::operator-(RealVariable& x,RealVariable& y){
-    
-    return x;
-}
-    
-RealVariable& solver::operator^(RealVariable& x,int y){
-    
-    return x;
 
-} 
-RealVariable& solver::operator==(RealVariable& x, const RealVariable& y) {
-    
-    return x;
-}
-RealVariable& solver::operator==(int x,RealVariable& y) {
-    
+RealVariable& solver::operator/(double x, RealVariable &y){
     return y;
 }
-RealVariable& solver::operator==(RealVariable& x, int y) {
-    
-    return x;
-    
+
+double solver::solve(RealVariable& x){
+    return 0;
 }
 
-/*Complex Operators*/
-const ComplexVariable& solver::operator+(const int y, const ComplexVariable& x)
-{
-    return x;
+
+/*********complex*********/
+
+ComplexVariable& ComplexVariable::operator+(std::complex<double> x){
+    return *this;
 }
-const ComplexVariable& solver::operator+(const ComplexVariable& x, const int y)
-{
-    return x;
-}
-const ComplexVariable& solver::operator+(const ComplexVariable& x, const ComplexVariable& y)
-{
-    return x;
-}
-const ComplexVariable& solver::operator+(const ComplexVariable& x, const std::complex<double>& y)
-{
-    return x;
-}
-const ComplexVariable& solver::operator+(const std::complex<double>& y, const ComplexVariable& x)
-{
-    return x;
+ComplexVariable& ComplexVariable::operator==(std::complex<double> x){
+    return *this;
 }
 
-const ComplexVariable& solver::operator*(const int y, const ComplexVariable& x)
-{
-    return x;
+ComplexVariable& ComplexVariable::operator+(ComplexVariable &x){
+    return *this;
 }
-const ComplexVariable& solver::operator*(const ComplexVariable& x, const int y)
-{
-    return x;
-}
-const ComplexVariable& solver::operator*(const ComplexVariable& x, const ComplexVariable& y)
-{
-    return x;
+ComplexVariable& ComplexVariable::operator==(ComplexVariable &x){
+    return *this;
 }
 
-const ComplexVariable& solver::operator/(const ComplexVariable& x, const int y)
-{
-    if(isZero(y)) throw runtime_error("Cannot divide by 0");
-    
-    return x;
-}
-const ComplexVariable& solver::operator/(const int y, const ComplexVariable& x)
-{        
-    return x;
-}
-const ComplexVariable& solver::operator/(const ComplexVariable& x, const ComplexVariable& y)
-{        
-    return x;
+ComplexVariable& ComplexVariable::operator-(std::complex<double> x){
+    return *this;
 }
 
-const ComplexVariable& solver::operator-(const ComplexVariable& x, const int y)
-{        
-    return x;
-}
-const ComplexVariable& solver::operator-(const ComplexVariable& x, const ComplexVariable& y)
-{        
-    return x;
-}
-const ComplexVariable& solver::operator-(const int y, const ComplexVariable& x)
-{        
-    return x;
+ComplexVariable& ComplexVariable::operator-(ComplexVariable &x){
+    return *this;
 }
 
-const ComplexVariable& solver::operator^(const ComplexVariable& x, const int y)
-{        
-    return x;
+ComplexVariable& ComplexVariable::operator*(std::complex<double> x){
+    return *this;
 }
 
-const ComplexVariable& solver::operator==(const ComplexVariable& x, const int y)
-{        
-    return x;
-}
-const ComplexVariable& solver::operator==(const RealVariable& y, const ComplexVariable& x)
-{        
-    return x;
-}
-const ComplexVariable& solver::operator==(const int y, const ComplexVariable& x)
-{        
-    return x;
-}
-const ComplexVariable& solver::operator==(const ComplexVariable y, const ComplexVariable& x)
-{        
-    return x;
+ComplexVariable& ComplexVariable::operator*(ComplexVariable &x){
+    return *this;
 }
 
+ComplexVariable& ComplexVariable::operator^(std::complex<double> x){
+    return *this;
+}
+
+ComplexVariable& ComplexVariable::operator^(ComplexVariable &x){
+    return *this;
+}
+
+ComplexVariable& ComplexVariable::operator/(std::complex<double> x){
+    return *this;
+}
+
+ComplexVariable& ComplexVariable::operator/(ComplexVariable &x){
+    return *this;
+}
+
+ComplexVariable& solver::operator==(std::complex<double> x, ComplexVariable &y){
+    return y;
+}
+
+ComplexVariable& solver::operator+(std::complex<double> x, ComplexVariable &y){
+    return y;
+}
+
+ComplexVariable& solver::operator-(std::complex<double> x, ComplexVariable &y){
+    return y;
+}
+
+ComplexVariable& solver::operator*(std::complex<double> x, ComplexVariable &y){
+    return y;
+}
+
+ComplexVariable& solver::operator/(std::complex<double> x, ComplexVariable &y){
+    return y;
+}
+
+std::complex<double> solver::solve(ComplexVariable& x){
+    return 0;
+}
--- solver.hpp
+++ solver.hpp
@@ -1,90 +1,92 @@
-/*
-*
-* Created by kalif on 24/04/2020.
-*
-*/
-
-#pragma once
 #include <iostream>
 #include <complex>
-#define EPS 0.0001
+#define EPS .0001
 using namespace std;
-template<typename T>
-double solve(T a);
+namespace solver {
 
-namespace solver
-{
-
-    class RealVariable
-    {   
+    bool isZero (double x);
+    class RealVariable {
     public:
-
         double _coef = 1;
-        int _power = 1;
+        double _power = 1;
+       
+        RealVariable& operator==(double x);
+
+        RealVariable& operator==(RealVariable &x);
+
+        RealVariable& operator+(double x);
+
+        RealVariable& operator+(RealVariable &x);
+
+        RealVariable& operator-(double x);
+
+        RealVariable& operator-(RealVariable &x);
+
+        RealVariable& operator*(double x);
+
+        RealVariable& operator*(RealVariable &x);
+
+        RealVariable& operator^(double x);
+
+        RealVariable& operator/(double x);
+
+        RealVariable& operator/(RealVariable &x);
 
     };
-    const double solve(RealVariable& x);
-    
 
-    RealVariable& operator+(const int y,RealVariable& x);
-    RealVariable& operator+(RealVariable& x,const int y);
-    RealVariable& operator+(RealVariable& x,RealVariable& y);
-    
-    RealVariable& operator*(const int y,RealVariable& x);
-    RealVariable& operator*(RealVariable& x,const int y);
-    RealVariable& operator*(RealVariable& x,RealVariable& y);
-    
-    RealVariable& operator/(RealVariable& x,const double y);
-    RealVariable& operator/(const double y,RealVariable& x);
-    RealVariable& operator/(RealVariable& x,RealVariable& y);
-    
-    RealVariable& operator-(RealVariable& x,const int y);
-    RealVariable& operator-(RealVariable& x,RealVariable& y);
-    RealVariable& operator-(const int y,RealVariable& x);
-    
-    RealVariable& operator^(RealVariable& x,const int y);
-    
-    RealVariable& operator==(RealVariable& x, const int y); 
-    RealVariable& operator==(RealVariable& x, const RealVariable& y);
-    RealVariable& operator==(const int y,RealVariable& x);
-   
-    class ComplexVariable
-    {
+    RealVariable& operator==(double x, RealVariable &y);
+
+    RealVariable& operator+(double x, RealVariable &y);
+
+    RealVariable& operator-(double x, RealVariable &y);
+
+    RealVariable& operator*(double x, RealVariable &y);
+
+    RealVariable& operator/(double x, RealVariable &y);
+
+
+    class ComplexVariable {
     public:
-        double _realPower;
-        double _imPower;
+        std::complex<double> coff = 1;
+        std::complex<double> power = 1;
+        
+        ComplexVariable& operator+(std::complex<double> x);
 
+        ComplexVariable& operator==(std::complex<double> x);
+
+        ComplexVariable& operator+(ComplexVariable &x);
+        
+        ComplexVariable& operator==(ComplexVariable &x);
+
+        ComplexVariable& operator-(std::complex<double> x);
+
+        ComplexVariable& operator-(ComplexVariable &x);
+
+        ComplexVariable& operator*(std::complex<double> x);
+
+        ComplexVariable& operator*(ComplexVariable &x);
+
+        ComplexVariable& operator^(std::complex<double> x);
+
+        ComplexVariable& operator^(ComplexVariable &x);
+
+        ComplexVariable& operator/(std::complex<double> x);
+
+        ComplexVariable& operator/(ComplexVariable &x);
     };
     
-    const std::complex<double> solve(ComplexVariable& x);
+    ComplexVariable& operator==(std::complex<double> x, ComplexVariable &y);
 
-    const ComplexVariable& operator+(const int y, const ComplexVariable& x);
-    const ComplexVariable& operator+(const ComplexVariable& x, const int y);
-    const ComplexVariable& operator+(const ComplexVariable& x, const ComplexVariable& y);
-    const ComplexVariable& operator+(const ComplexVariable& x, const std::complex<double>& y);
-    const ComplexVariable& operator+(const std::complex<double>& y, const ComplexVariable& x);
-    
-    const ComplexVariable& operator*(const int y, const ComplexVariable& x);
-    const ComplexVariable& operator*(const ComplexVariable& x, const int y);
-    const ComplexVariable& operator*(const ComplexVariable& x, const ComplexVariable& y);
-    
-    const ComplexVariable& operator/(const ComplexVariable& x, const int y);
-    const ComplexVariable& operator/(const int y, const ComplexVariable& x);
-    const ComplexVariable& operator/(const ComplexVariable& x, const ComplexVariable& y);
-    
-    const ComplexVariable& operator-(const ComplexVariable& x, const int y);
-    const ComplexVariable& operator-(const ComplexVariable& x, const ComplexVariable& y);
-    const ComplexVariable& operator-(const int y, const ComplexVariable& x);
-    
-    const ComplexVariable& operator^(const ComplexVariable& x, const int y);
-    
-    const ComplexVariable& operator==(const ComplexVariable& x, const int y); 
-    const ComplexVariable& operator==(const RealVariable& x, const ComplexVariable& y);
-    const ComplexVariable& operator==(const int y, const ComplexVariable& x);
-    const ComplexVariable& operator==(const ComplexVariable y, const ComplexVariable& x);
+    ComplexVariable& operator+(std::complex<double> x, ComplexVariable &y);
 
-    bool isZero (double num);
+    ComplexVariable& operator-(std::complex<double> x, ComplexVariable &y);
 
+    ComplexVariable& operator*(std::complex<double> x, ComplexVariable &y);
 
-    
-} 
+    ComplexVariable& operator/(std::complex<double> x, ComplexVariable &y);
+
+    double solve(RealVariable& x);
+
+    std::complex<double> solve(ComplexVariable& x);
+
+};
